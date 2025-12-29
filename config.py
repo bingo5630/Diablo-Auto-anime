63a05ced9b71ffd5e5b021efe8eae0921c73c607
bingo5630 <binghh5@gmail.com>
2025-12-29 21:23:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 from os import getenv
 from dotenv import load_dotenv
 import logging
@@ -6,6 +5,24 @@ from logging.handlers import RotatingFileHandler
 
 load_dotenv()
 LOGS = logging.getLogger(__name__)
+
+# =========================
+# SHORTNER CONFIG
+# =========================
+SHORT_URL = getenv("SHORT_URL", "linkshortify.com")   # your shortener base url
+SHORT_API = getenv("SHORT_API", "")                   # your API key / token
+
+MESSAGES = {
+    "START": "<b>›› ʜᴇʏ!!, {first} ~ <blockquote>ʟᴏᴠᴇ ᴘᴏʀɴʜᴡᴀ? ɪ ᴀᴍ ᴍᴀᴅᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ғɪɴᴅ ᴡʜᴀᴛ ʏᴏᴜ aʀᴇ ʟᴏᴏᴋɪɴɢ ꜰᴏʀ.</blockquote></b>",
+    "FSUB": "<b><blockquote>›› ʜᴇʏ ×</blockquote>\n  ʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴀᴅʏ ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ sᴜʙsᴄʀɪʙᴇᴅ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ, sᴜʙsᴄʀɪʙᴇ ɴᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ғɪʟᴇs</b>",
+    "ABOUT": "<b>›› ғᴏʀ ᴍᴏʀᴇ: @Nova_Flix \n <blockquote expandable>›› ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/codeflix_bots'>Cʟɪᴄᴋ ʜᴇʀᴇ</a> \n›› ᴏᴡɴᴇʀ: @ProYato\n›› ʟᴀɴɢᴜᴀɢᴇ: <a href='https://docs.python.org/3/'>Pʏᴛʜᴏɴ 3</a> \n›› ʟɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ ᴠ2</a> \n›› ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>Mᴏɴɢᴏ ᴅʙ</a> \n›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @cosmic_freak</b></blockquote>",
+    "REPLY": "<b>For More Join - @Hanime_Arena</b>",
+    "SHORT_MSG": "<b>📊 ʜᴇʏ {first}, \n\n‼️ ɢᴇᴛ ᴀʟʟ ꜰɪʟᴇꜱ ɪɴ ᴀ ꜱɪɴɢʟᴇ ʟɪɴᴋ ‼️\n\n ⌯ ʏᴏᴜʀ ʟɪɴᴋ ɪꜱ ʀᴇᴀᴅʏ, ᴋɪɴᴅʟʏ ᴄʟɪᴄᴋ ᴏɴ ᴏᴘᴇɴ ʟɪɴᴋ ʙᴜᴛᴛᴏɴ..</b>",
+    "START_PHOTO": "https://graph.org/file/510affa3d4b6c911c12e3.jpg",
+    "FSUB_PHOTO": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
+    "SHORT_PIC": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
+    "SHORT": "https://telegra.ph/file/8aaf4df8c138c6685dcee-05d3b183d4978ec347.jpg"
+}
 
 class Var:
     API_ID = getenv("API_ID", "26944587")
@@ -20,7 +37,7 @@ class Var:
     ANIME = getenv("ANIME", "Is It Wr2131ong to Try to Pi123ck Up Girls in a Dungeon?")
     CUSTOM_BANNER = getenv("CUSTOM_BANNER", "https://graph.org/file/2bc85a7390dfb95c50cde-9ef99a8c985013ee00.jpg")
 
-    PROTECT_CONTENT = True if getenv('PROTECT_CONTENT', "False") == "True" else False 
+    PROTECT_CONTENT = True if getenv('PROTECT_CONTENT', "False") == "True" else False
     BACKUP_CHANNEL = int(getenv("BACKUP_CHANNEL", "0"))
     LOG_CHANNEL = int(getenv("LOG_CHANNEL", "-1003383572706"))
     MAIN_CHANNEL = int(getenv("MAIN_CHANNEL", " -1002666104359"))
@@ -45,14 +62,12 @@ class Var:
     FORCE_PIC = getenv("FORCE_PIC", "https://graph.org/file/a57921335f973d9620b3b-f2c446e02a107bd6c1.jpg")
 
 
-# ✅ Required variable validation (outside the class)
+# Required vars check
 REQUIRED_VARS = ["API_ID", "API_HASH", "BOT_TOKEN", "DB_URI"]
 for var_name in REQUIRED_VARS:
     if not getattr(Var, var_name):
         LOGS.critical(f"Missing required environment variable: {var_name}")
         exit(1)
-        #--------------------------------------------
-
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
@@ -74,3 +89,4 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+    
